9abcac3f7bece85cb6c5b4df5b21c4d953695590
w00kie <112516618+w00k1337@users.noreply.github.com>
2025-07-18 14:57:04 +0200

    chore: add initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactElement, ReactNode } from 'react'
+
+interface RootLayoutProps {
+  children: ReactNode
+}
+
+const RootLayout = ({ children }: RootLayoutProps): ReactElement => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+import { ReactElement } from 'react'
+
+const HomePage = (): ReactElement => {
+  return (
+    <div className="m-0 flex min-h-screen items-center justify-center p-0">
+      <h1 className="m-0 text-5xl font-bold tracking-tight">DLicious</h1>
+    </div>
+  )
+}
+
+export default HomePage
