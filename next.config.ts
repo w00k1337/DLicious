53be840111c497aa7050de8403aa7db201135395
w00kie <112516618+w00k1337@users.noreply.github.com>
2025-07-26 20:12:49 +0200

    chore: add 'bullmq' to serverExternalPackages in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  serverExternalPackages: ['pino']
+  serverExternalPackages: ['bullmq', 'pino']
 }
 
 export default nextConfig
